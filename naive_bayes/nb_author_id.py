9261b00d4783e32c83380afd5eaa452a9edd4ba9
Tegan Lohman <tmlohman@gmail.com>
2016-07-14 12:41:53 -0700

    time training and prediction

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -38,15 +38,18 @@ def classify(features_train, labels_train):
     return clf.fit(features_train, labels_train)
 
 #train classifier
+t0 = time()
 clf = classify(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
 
 #predict outcome
+t0 = time()
 pred = clf.predict(features_test)
+print "prediction time:", round(time()-t0, 3), "s"
 
 #check accuracy
 accuracy = accuracy_score(labels_test, pred)
 print accuracy
-
 
 
 #########################################################
