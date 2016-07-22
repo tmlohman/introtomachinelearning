2fe90d25f9a57d6808e518337fb45f37d502f61a
Tegan Lohman <tmlohman@gmail.com>
2016-07-22 13:59:19 -0700

    complete decision tree mini project

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -12,6 +12,7 @@ import sys
 from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
+from sklearn.metrics import accuracy_score
 
 
 ### features_train and features_test are the features for the training
@@ -23,7 +24,27 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 
 #########################################################
-### your code goes here ###
+def classify(features_train, labels_train):   
+    from sklearn import tree
+    clf = tree.DecisionTreeClassifier(min_samples_split = 40)
+    #features_train = features_train[:len(features_train)/100] 
+    #labels_train = labels_train[:len(labels_train)/100] 
+    return clf.fit(features_train, labels_train)
+
+#train classifier
+t0 = time()
+clf = classify(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
+
+#predict outcome
+t0 = time()
+pred = clf.predict(features_test)
+print "prediction time:", round(time()-t0, 3), "s"
+
+#check accuracy
+accuracy = accuracy_score(labels_test, pred)
+print accuracy
+print len(features_train[0])
 
 
 #########################################################
